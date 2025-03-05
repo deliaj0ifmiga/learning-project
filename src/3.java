0c39ef2a6991d5fabadb8492b7378333fb97e1aa
deliaj0ifmiga <deliaj0ifmiga@gmx.com>
2025-03-05 14:23:45 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Java {
+	public static void main(String args[]){
+		int x = 42;
+		if(x == 13){
+			System.out.println("Hello World");
+		}else if (x > 50){
+			System.out.println("This is a long string that goes on and on and on and on and on and on");
+		}else{
+			System.out.println("The number is not 13 or greater than 50");
+		}
+		System.out.println("The number is: " + x);
+	}
+}
