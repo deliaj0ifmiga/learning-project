df8a7c45fc840267e3c266bcbcab23bce3b601f1
deliaj0ifmiga <deliaj0ifmiga@gmx.com>
2025-03-08 15:10:33 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,9 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int num = (int)(Math.random() * 10) + 1;
+
+        // Print the result
+        System.out.println("Random number: " + num);
+    }
+}
